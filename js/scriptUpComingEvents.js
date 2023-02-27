88d0717b7befef96195eddc6b841f8cc5eb5ea36
Edu578 <eduardo.arevalo072@gmail.com>
2023-02-27 09:54:46 -0300

    Se suben cambios para revision

--- js/scriptUpComingEvents.js
+++ js/scriptUpComingEvents.js
@@ -10,9 +10,10 @@ function createUpComingEvents(arrayData){
                 <div class="card" style="width: 18rem;">
                     <img src="${event.image}" class="card-img-top" alt=${event.name}>
                     <div class="card-body">
-                    <h5 class="card-title">${event.name}</h5>
-                    <p class="card-text">${event.description}</p>
-                    <a href="details.html" class="btn btn-primary">Get Details</a>
+                        <h5 class="card-title">${event.name}</h5>
+                        <p class="card-text">${event.description}</p>
+                        <a href="details.html" class="btn btn-primary">Get Details</a>
+                    </div>
                 </div>
                 <div class="card-footer pt-3 pb-3 d-flex justify-content-around align-items-xl-baseline">
                     <p class="mb-0 d-flex flex-row flex-md-column flex-xl-row">
