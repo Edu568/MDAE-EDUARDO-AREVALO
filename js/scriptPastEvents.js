25513051f537661f6c150b42e683e6e31b66dc0e
Eduardo Arevalo <eduardo.arevalo072@gmail.com>
2023-03-06 20:55:34 -0300

    se suben nuevos cambios en cards

--- js/scriptPastEvents.js
+++ js/scriptPastEvents.js
@@ -10,20 +10,21 @@ function createPastEvents(arrayData){
                 <div class="card" style="width: 18rem;">
                     <img src="${event.image}" class="card-img-top" alt=${event.name}>
                     <div class="card-body">
-                    <h5 class="card-title">${event.name}</h5>
-                    <p class="card-text">${event.description}</p>
-                    <a href="details.html" class="btn btn-primary">Get Details</a>
+                        <h5 class="card-title">${event.name}</h5>
+                        <p class="card-text">${event.description}</p>
+                        <a href="details.html" class="btn btn-primary">Get Details</a>
+                    </div>
                 </div>
-            </div>
-            <div class="card-footer pt-3 pb-3 d-flex justify-content-around align-items-xl-baseline">
-                <p class="mb-0 d-flex flex-row flex-md-column flex-xl-row">
-                    <span>
-                        <i class="bi bi-tag"></i>
-                        price:&nbsp;
-                    </span>
-                    ${event.price}
-                </p>
+                <div class="card-footer pt-3 pb-3 d-flex justify-content-around align-items-xl-baseline">
+                    <p class="mb-0 d-flex flex-row flex-md-column flex-xl-row">
+                        <span>
+                            <i class="bi bi-tag"></i>
+                            price:&nbsp;
+                        </span>
+                        $${event.price}
+                    </p>
                 
+                </div>
             </div>
             `
         }
